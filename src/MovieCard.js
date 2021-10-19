397a4a25cd4794665ce263848d70435e9d9e40a3
RizkyMaulita <rizky.maulita@gmail.com>
2021-10-19 18:21:18 +0700

    add class component in moviecard.js

---
+++ src/MovieCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class MovieCard extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        this is class component for movie card
+      </div>
+    )
+  }
+}
+
+export default MovieCard
