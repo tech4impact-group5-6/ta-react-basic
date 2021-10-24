3c6309a37658cd99bc9011b8dcbe3c70aa1d29d5
deviayulestari <devialestari04@gmail.com>
2021-10-25 00:40:19 +0700

    add answer soal-02

--- Soal-02/src/App.js
+++ Soal-02/src/App.js
@@ -1,9 +1,34 @@
 import { Component } from "react";
-import animeList from "./dummy-data";
+// import animeList from "./dummy-data";
 import MovieCard from "./MovieCard";
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      movieList: [],
+      isLoading: true
+    }
+  }
+
+  componentDidMount(){
+    setTimeout(() => this.fetchMovieList(), 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.movieList.length !== this.state.movieList.length) {
+      this.setState({isLoading: false});
+    }
+  }
+
+  fetchMovieList(){
+    fetch("https://api.jikan.moe/v3/top/anime")
+      .then(response => response.json())
+      .then(data => console.log(this.setState({movieList: data?.top})))
+      .catch(err => console.log(err))
+  }
+
   render() {
     // console.log(animeList);
     return (
@@ -12,67 +37,12 @@ class App extends Component {
         <div className="container my-5">
           <div id="daftar-anime" className="row">
             {
-              animeList?.map((movie) =>(
-                <MovieCard key={movie?.mal_id} movie={movie}></MovieCard>
-              ))
-            }
-          
-
-          {/* <div className="col-6 my-3">
-              <div class="card h-100">
-                <img
-                  src="https://cdn.myanimelist.net/images/anime/1223/96541.jpg?s=faffcb677a5eacd17bf761edd78bfb3f"
-                  class="card-img-top"
-                  alt="skilvul"
-                />
-                <div class="card-body">
-                  <h5 class="card-title">Fullmetal Alchemist: Brotherhood</h5>
-                  <h6 class="card-subtitle mb-2">
-                    <span class={`badge bg-danger`}>TV</span>
-                  </h6>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-                <div class="card-body">
-                  <a
-                    href="https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood"
-                    class="btn btn-primary w-100 text-uppercase"
-                  >
-                    readmore
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-6 my-3">
-              <div class="card h-100">
-                <img
-                  src="https://cdn.myanimelist.net/images/anime/1245/116760.jpg?s=8f52d6ff46ef1233eb9fcd542434221c"
-                  class="card-img-top"
-                  alt="skilvul"
-                />
-                <div class="card-body">
-                  <h5 class="card-title">Gintama: The Final</h5>
-                  <h6 class="card-subtitle mb-2">
-                    <span class={`badge bg-success`}>MOVIE</span>
-                  </h6>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-                <div class="card-body">
-                  <a
-                    href="https://myanimelist.net/anime/39486/Gintama__The_Final"
-                    class="btn btn-primary w-100 text-uppercase"
-                  >
-                    readmore
-                  </a>
-                </div>
-              </div>
-            </div>
-          */}
+              this.state.isLoading ? (<h2>Loading...</h2>) 
+              : (this.state.movieList.map((movie) => (
+                  <MovieCard key={movie?.mal_id} movie={movie}></MovieCard>
+                ))
+              )
+            }     
           </div> 
         </div>
       </div>
--- Soal-02/src/MovieCard.js
+++ Soal-02/src/MovieCard.js
@@ -9,41 +9,39 @@ export default class MovieCard extends Component{
     render(){
       console.log(this.props);
         return(
-            <>
-            <div className="col-6 my-3">
-              <div class="card h-100">
+          <>
+            <div className="col-lg-3 col-md-4 col-sm-6 my-3">
+              <div className="card h-100">
                 <img
                   src={this.props?.movie?.image_url}
-                  class="card-img-top"
+                  className="card-img-top"
                   alt="skilvul"
                 />
-                <div class="card-body">
-                  <h5 class="card-title">{this.props?.movie?.title}</h5>
-                  <h6 class="card-subtitle mb-2">
-                    {/* {this.props?.movie?.type == "TV" ? `badge bg-danger`: `badge bg-success`} */}
-                    {/* <span class={`badge bg-danger`}>TV</span> */}
+                <div className="card-body">
+                  <h5 className="card-title">{this.props?.movie?.title}</h5>
+                  <h6 className="card-subtitle mb-2">
                     {
                       this.props?.movie?.type === "TV"
-                      ? <span class={`badge bg-danger`}>{this.props?.movie?.type}</span>
-                      : <span class={`badge bg-success`}>{this.props?.movie?.type}</span>
+                      ? <span className={`badge bg-danger`}>{this.props?.movie?.type}</span>
+                      : <span className={`badge bg-success`}>{this.props?.movie?.type}</span>
                     }
                   </h6>
-                  <p class="card-text">
+                  <p className="card-text">
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
                   </p>
                 </div>
-                <div class="card-body">
+                <div className="card-body">
                   <a
                     href={this.props?.movie.url}
-                    class="btn btn-primary w-100 text-uppercase"
+                    className="btn btn-primary w-100 text-uppercase"
                   >
                     readmore
                   </a>
                 </div>
               </div>
             </div>
-            </>
+          </>
         )
     }
 }
