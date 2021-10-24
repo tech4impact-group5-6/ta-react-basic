869542d7501817a92febfb50c95af4def8032337
salvayuca <salvayuca88@gmail.com>
2021-10-25 01:30:32 +0700

    assignments done

---
+++ Soal_1/src/MovieCard.js
@@ -0,0 +1,55 @@
+import { Component } from "react";
+
+class MovieCard extends Component {
+    movie = {};
+
+    constructor(props) {
+        super(props);
+        this.movie = props.movie;
+        this.connection = props.lain;
+    }
+
+    render() {
+      return (
+        <div className="col-12 col-lg-3 col-md-4 col-sm-6 my-3">
+            {this.connection}
+            <div className="card h-100">
+                <img
+                    src={this.props?.movie?.image_url}
+                    className="card-img-top"
+                    alt="skilvul"
+                />
+                <div className="card-body">
+                    <h5 className="card-title">{this.props?.movie?.title}</h5>
+                    <h6 class="card-subtitle mb-2">
+                        {this.props?.movie?.type === "TV" ? (
+                            <span class={`badge bg-danger`}>
+                                {this.props?.movie?.type}
+                            </span>
+                        ) : (
+                            <span class={`badge b-success`}>
+                                {this.props?.movie?.type}
+                            </span>
+                        )}
+                    </h6>
+                    <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                    </p>
+                </div>
+                <div className="card-body">
+                    <a
+                    href={this.props?.movie?.url}
+                    target="_blank"
+                    className="btn btn-primary w-100 text-uppercase"
+                    >
+                    readmore
+                    </a>
+                </div>
+            </div>
+        </div>
+      )
+    }
+}
+
+export default MovieCard;
--- Soal_2/src/App.js
+++ Soal_2/src/App.js
@@ -1,67 +1,47 @@
 import { Component } from "react";
-import animeList from "./dummy-data";
+// import animeList from "./dummy-data";
+import MovieCard from "./MovieCard";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieList: [],
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.fetchMovieList(), 2000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.movieList.length !== this.state.movieList.length) {
+      this.setState({
+        isLoading:  false
+      });
+    }
+  }
+
+  async fetchMovieList() {
+    const response = await fetch('https://api.jikan.moe/v3/top/anime');
+    const result = await response.json();
+    this.setState({
+      movieList: result?.top })
+  }
+
   render() {
     return (
       <div className="container my-5">
         <h1 className="text-center">Anime List SOAL 02</h1>
         <div className="container my-5">
           <div id="daftar-anime" className="row">
-            <div className="col-6 my-3">
-              <div class="card h-100">
-                <img
-                  src="https://cdn.myanimelist.net/images/anime/1223/96541.jpg?s=faffcb677a5eacd17bf761edd78bfb3f"
-                  class="card-img-top"
-                  alt="skilvul"
-                />
-                <div class="card-body">
-                  <h5 class="card-title">Fullmetal Alchemist: Brotherhood</h5>
-                  <h6 class="card-subtitle mb-2">
-                    <span class={`badge bg-danger`}>TV</span>
-                  </h6>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-                <div class="card-body">
-                  <a
-                    href="https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood"
-                    class="btn btn-primary w-100 text-uppercase"
-                  >
-                    readmore
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-6 my-3">
-              <div class="card h-100">
-                <img
-                  src="https://cdn.myanimelist.net/images/anime/1245/116760.jpg?s=8f52d6ff46ef1233eb9fcd542434221c"
-                  class="card-img-top"
-                  alt="skilvul"
-                />
-                <div class="card-body">
-                  <h5 class="card-title">Gintama: The Final</h5>
-                  <h6 class="card-subtitle mb-2">
-                    <span class={`badge bg-success`}>MOVIE</span>
-                  </h6>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                </div>
-                <div class="card-body">
-                  <a
-                    href="https://myanimelist.net/anime/39486/Gintama__The_Final"
-                    class="btn btn-primary w-100 text-uppercase"
-                  >
-                    readmore
-                  </a>
-                </div>
-              </div>
-            </div>
+            {
+              this.state.isLoading ? (<h1>Loading...</h1>) :
+              this.state.movieList.map((movie) =>
+                <MovieCard key={movie?.mal_id} movie={movie}></MovieCard>
+                )
+            }
           </div>
         </div>
       </div>
