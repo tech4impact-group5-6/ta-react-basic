401c1ac5f8532c543819dc085feba366a8993d07
Arvel Gavrilla R <arvel.gavrilla@gmail.com>
2021-10-25 08:40:28 +0700

    tyding up some code, again

--- Soal_1/src/MovieCard.js
+++ Soal_1/src/MovieCard.js
@@ -18,11 +18,11 @@ class MovieCard extends Component {
                 <div className="card-body">
                     <h5 className="card-title">{this.movie.title}</h5>
                     <h6 className="card-subtitle mb-2">
-                    <span className={`badge ${this.movie.type === "TV" ? 'bg-danger' : 'bg-success'}`}>{this.movie.type}</span>
+                        <span className={`badge ${this.movie.type === "TV" ? 'bg-danger' : 'bg-success'}`}>{this.movie.type}</span>
                     </h6>
                     <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                        Some quick example text to build on the card title and make
+                        up the bulk of the card's content.
                     </p>
                 </div>
                 <div className="card-body">
