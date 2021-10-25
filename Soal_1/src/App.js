d34fefdf57da29d23f6ed19604f76ebe4b6c0e79
Arvel Gavrilla R <arvel.gavrilla@gmail.com>
2021-10-25 09:00:46 +0700

    update app.js

--- Soal_1/src/App.js
+++ Soal_1/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="container my-5">
-        <h1 className="text-center">Anime List</h1>
+        <h1 className="text-center">Anime List SOAL 01</h1>
         <div className="container my-5">
           <div id="daftar-anime" className="row">
             {
